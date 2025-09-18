07edf94020693d4dc6c717e794961ccb723ca1c3
dandre3000 <deaundre.payne@gmail.com>
2025-09-17 22:35:13 -0400

    v0.1.2

--- performance.js
+++ performance.js
@@ -1,7 +1,7 @@
-import { clear, getEntries, mark, stop } from 'marky'
+import { ListNode } from './List.ts'
 // package with high downloads that exports a linked list node class
 import { List as LinkedList, Item } from 'linked-list'
-import { ListNode } from './List.ts'
+import { Suite } from 'benchmark'
 
 class MyItem extends Item {
     constructor (value) {
@@ -11,40 +11,71 @@ class MyItem extends Item {
     }
 }
 
-const values = new Array(10e4 * 2.5).fill(0).map(Math.random)
+const values = new Array(10e5).fill(0).map(Math.random)
 const nodes = []
 const items = []
 
-for (let i = 0; i < values.length; i++) {
-    items[i] = new MyItem(values[i])
-    nodes[i] = new ListNode(values[i])
-}
+let suite1 = new Suite
 
-mark('0')
-mark('0')
-clear()
-
-setTimeout(() => {
-    mark('Item.prototype.append')
-
-    let i = 0
-    new LinkedList().prepend(items[i])
-
-    for (i = 0; i < items.length - 1; i++) {
-        items[i].append(items[i + 1])
+suite1.add('Item.prototype.constructor', () => {
+    for (let i = 0; i < values.length; i++) {
+        items[i] = new MyItem(values[i])
     }
-
-    stop('Item.prototype.append')
-
-    setTimeout(() => {
-        mark('ListNode.prototype.appendTo')
-
-        for (let i = 0; i < nodes.length - 1; i++) {
-            nodes[i + 1].appendTo(nodes[i])
-        }
-    
-        stop('ListNode.prototype.appendTo')
-
-        setTimeout(() => getEntries().forEach(({ name, duration }) => console.log(name, duration)))
-    })
 })
+.add('ListNode.prototype.constructor', () => {
+    for (let i = 0; i < values.length; i++) {
+        nodes[i] = new ListNode(values[i])
+    }
+})
+.on('cycle', function(event) {
+    console.log(String(event.target))
+})
+.on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+
+    suite2.run({ 'async': true })
+})
+
+let suite2 = new Suite
+
+suite2.add('Item.prototype.prepend', () => {
+    new LinkedList().prepend(items[0])
+
+    for (let i = 0; i < items.length >>> 1; i++) {
+        items[i].prepend(items[i + 1])
+    }
+})
+.add('ListNode.prototype.prependTo', () => {
+    for (let i = 0; i < nodes.length >>> 1; i++) {
+        nodes[i + 1].prependTo(nodes[i])
+    }
+})
+.on('cycle', function(event) {
+    console.log(String(event.target))
+})
+.on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+
+    suite3.run({ 'async': true })
+})
+
+let suite3 = new Suite
+
+suite3.add('Item.prototype.append', () => {
+    for (let i = items.length >>> 1 + 1; i < items.length; i++) {
+        items[0].append(items[i])
+    }
+})
+.add('ListNode.prototype.appendTo', () => {
+    for (let i = nodes.length >>> 1 + 1; i < nodes.length; i++) {
+        nodes[i].appendTo(nodes[0])
+    }
+})
+.on('cycle', function(event) {
+    console.log(String(event.target))
+})
+.on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+})
+
+suite1.run({ 'async': true })
